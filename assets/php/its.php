a0c64cc9f0cb905fbb3b7c5432bd8367a5aaa8e9
novachloe <novamaechloegabalesveloz@gmail.com>
2019-08-01 13:37:40 +0800

    updated

---
+++ assets/php/its.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+    function writeMsg() {
+        echo "Hello world!";
+    }
+    
+    writeMsg();
+    ?>
+
+<a href="./assets/php/its.php/index.html"></a>
+<script src="./assets/js/script.js"></script>
+</body>
+</html>
